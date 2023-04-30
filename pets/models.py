b35406f2dba876b8f3009e371526e5db6ce0b2d3
Victoria <victoria-milan@outlook.com>
2023-04-30 18:59:15 -0300

    feat/ create models

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class PetSexOptions(models.TextChoices):
+    MALE = "Male"
+    FEMALE = "Female"
+    DEFAULT = "Not Informed"
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    weight = models.FloatField()
+    sex = models.CharField(max_length=20, choices=PetSexOptions.choices, default=PetSexOptions.DEFAULT)
+
+    group = models.ForeignKey("groups.Group", on_delete=models.PROTECT, related_name="pets", null=True)
+    traits = models.ManyToManyField("traits.Trait", related_name="pets")
